fb6e495304fc5acdc4f47b7644387ecf778ede00
Natália Caroline <nataliadeolive@gmail.com>
2020-08-06 08:45:41 -0300

    interface finalizada e inicio de animacao da aplicacao

---
+++ scripts/addField.js
@@ -0,0 +1,19 @@
+//Procurar o botao
+document.querySelector("#add-time")
+//Quando clicar no botao
+.addEventListener('click',cloneField)
+//Executar uma acao
+function cloneField() {
+//Duplicar os campos.Quais campos?
+const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)// boolean: true ou false
+//pegar os campos. que campos?
+const fields = newFieldContainer.querySelectorAll('input')
+//para cada campo, limpar
+fields.forEach(function(field) {
+    //pega o field do momento e limpa ele
+    field.value = ""
+})
+
+//colocar na página: onde??
+document.querySelector('#schedule-items').appendChild(newFieldContainer)
+}
